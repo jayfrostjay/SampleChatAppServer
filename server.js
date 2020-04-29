5262c1ed77074b1565db25b1929ba45657f6b1f1
Jayson Garcia <jayfrostgarcia@gmail.com>
2020-04-30 02:25:10 +0800

    Update server.js

--- server.js
+++ server.js
@@ -141,10 +141,10 @@ io.on('connection', (socket) => {
 })
 
 const appServer = http.listen(port, () => {
-  console.log('server is running on port', appServer.address().port);
+  // console.log('server is running on port', appServer.address().port);
 });
 
 
 mongoose.connect(dbUrl ,{useNewUrlParser: true, useUnifiedTopology: true} ,(err) => {
-  console.log('mongodb connected',err);
+  // console.log('mongodb connected',err);
 })
