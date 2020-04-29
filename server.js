f99576d13129428452b69276a2890ad51f21bce8
Jayson Garcia <jayfrostgarcia@gmail.com>
2020-04-30 04:33:46 +0800

    update on mongoose

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const bodyParser = require('body-parser')
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const PORT = process.env.port || 3000
+
+const dbUrl = process.env.MONGODB_URI  || 'mongodb+srv://jayson:U7sJsYp6cVQ217AW@chatapp-hop1k.mongodb.net/chatapp?retryWrites=true&w=majority'
+mongoose.connect(dbUrl,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+mongoose.connection.on('connected', () => {
+  console.log('Mongoose is connected!!!!');
+})
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -20,8 +31,6 @@ const Login = mongoose.model('users', {
   password: String,
 })
 
-// //pass: U7sJsYp6cVQ217AW
-// const dbUrl = "mongodb+srv://jayson:U7sJsYp6cVQ217AW@chatapp-hop1k.mongodb.net/chatapp?retryWrites=true&w=majority"
 const defaultErrorResponse  = (err) => {
   return {
     status: false,
@@ -137,42 +146,10 @@ io.on('connection', (socket) => {
     const savedMessage = await message.save()
     io.emit('message', message)
   })
-})
+}) 
 
+app.listen(PORT, console.log(`Server is starting at ${PORT}`));
 
-const port = process.env.port || 3000
-const appServer = http.listen(port, () => {
-  console.log('server is running on port', appServer.address().port);
-})
-
-// mongoose.connect(process.env.MONGODB_URI ,{useNewUrlParser: true, useUnifiedTopology: true} ,(err) => {
-//   console.log('mongodb connected',err);
-// })
-
-var mongodbUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb+srv://jayson:U7sJsYp6cVQ217AW@chatapp-hop1k.mongodb.net/chatapp?retryWrites=true&w=majority"
-mongoose.connect(mongodbUri, {useNewUrlParser: true, useUnifiedTopology: true}, () => {
-  console.log('mongodb connected');
-})
-
-// const uri = `mongodb+srv://${process.env.DB_HOST}`;
-// const options = {
-//   user: process.env.DB_USER,
-//   pass: process.env.DB_PASS,
-//   dbName: process.env.DB_NAME,
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false,
-//   useCreateIndex: true
-// };
-// mongoose.connect(uri, options).then(
-//   _ => {
-//     console.info('Database connection established');
-//     http.listen(port, function() {
-//       console.log(`listening on port ${port}`);
-//     });
-//   },
-//   error => {
-//     console.error('Database connection failed:', error);
-//     throw new Error('Could not connect to the database');
-//   }
-// );
+// const appServer = http.listen(port, () => {
+//   console.log('server is running on port', appServer.address().port);
+// });
